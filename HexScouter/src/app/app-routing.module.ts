e31c14c7700da73d7322b44e0ce0a44758458864
Pineapple42 <frozenfire71@gmail.com>
2020-06-12 11:03:44 -0500

    Started App
    HexScouterDead is the old version. I don't like that template. The new one works a little better for me.

--- HexScouter/src/app/app-routing.module.ts
+++ HexScouter/src/app/app-routing.module.ts
@@ -4,15 +4,17 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
-    pathMatch: 'full'
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    path: 'tab4',
+    loadChildren: () => import('./tab4/tab4.module').then( m => m.Tab4PageModule)
+  },
+  {
+    path: 'tab5',
+    loadChildren: () => import('./tab5/tab5.module').then( m => m.Tab5PageModule)
   }
 ];
-
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
