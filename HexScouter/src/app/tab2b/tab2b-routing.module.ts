010f1c49e531c610d9fd2d6e6f48424db683cb31
Pineapple42 <frozenfire71@gmail.com>
2020-06-12 13:30:43 -0500

    Routing half-works
    Still not sure why the buttons stop working after the first time around

--- HexScouter/src/app/tab2b/tab2b-routing.module.ts
+++ HexScouter/src/app/tab2b/tab2b-routing.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import { Tab2Page } from '../tab2/tab2.page';
 import { Tab2bPage } from './tab2b.page';
+import { Tab2cPage } from '../tab2c/tab2c.page';
+import { Tab2dPage } from '../tab2d/tab2d.page';
+import { Tab2ePage } from '../tab2e/tab2e.page';
 
 const routes: Routes = [
   {
     path: '',
     component: Tab2bPage
-  }
+  },
+  { path: 'tab2', component: Tab2Page },
+  { path: 'tab2b', component: Tab2bPage },
+  { path: 'tab2c', component: Tab2cPage },
+  { path: 'tab2d', component: Tab2dPage },
+  { path: 'tab2e', component: Tab2ePage },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
 export class Tab2bPageRoutingModule {}
