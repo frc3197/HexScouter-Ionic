3ab8edb5a52ca0035bf5efa6ebec4401c586d625
Pineapple42 <frozenfire71@gmail.com>
2020-06-14 15:03:24 -0500

    General UI cleanup
    Added colors
    Home screen has been updated

--- HexScouter/src/app/tab2/tab2.page.ts
+++ HexScouter/src/app/tab2/tab2.page.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Plugins, FilesystemDirectory, FilesystemEncoding } from '@capacitor/core';
+import { ToastController } from '@ionic/angular';
+
+const { Filesystem } = Plugins;
 
 @Component({
   selector: 'app-tab2',
@@ -7,7 +11,54 @@ import { Router } from '@angular/router';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  teamName: String = "";
+  matchNum: String = "";
+  
+  constructor(public toastController: ToastController) {}
+  
+  async saveJSON(){
+    var obj = {teamName: this.teamName, matchNum: this.matchNum};
+    if(obj.matchNum == ""){
+      this.presentToast('Please Input Match Number');
+    }else if(obj.teamName == ""){
+      this.presentToast('Please Input Team Name');
+    }else{
+      try {
+        const result = await Filesystem.writeFile({
+          path: 'input/Match_' + this.matchNum + '.json',
+          data: JSON.stringify(obj),
+          directory: FilesystemDirectory.Data,
+          encoding: FilesystemEncoding.UTF8
+        })
+        console.log('Wrote file', result);
+        this.presentToast('Successfully Saved Data');
+      } catch(e){
+        this.presentToast('An Error Occured');
+        console.error('Unable to write file', e);
+      }
+    }
+  }
 
-  constructor() {}
+  //TODO: Only for Debug. Eventually should deprecate.
+  async readJSON(){
+    try{
+      let contents = await Filesystem.readFile({
+        path: 'input/Match_' + this.matchNum + '.json',
+        directory: FilesystemDirectory.Data,
+        encoding: FilesystemEncoding.UTF8
+      });
+      console.log(contents);
+    }catch(e){
+      this.presentToast('Please Input Match Number');
+      console.error('Match Number not Present', e);
+    }
+  }
 
+  async presentToast(m: String){
+    const toast = await this.toastController.create({
+      message: m.toString(),
+      duration: 2000,
+    });
+    toast.present();
+  }
 }
